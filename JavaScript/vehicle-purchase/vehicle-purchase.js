bf23917af3ab9ba314fdb4789760290eaa9926c6
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-05-11 18:08:46 +0430

    Solve the Vehicle purchase exercise

--- JavaScript/vehicle-purchase/vehicle-purchase.js
+++ JavaScript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,8 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return kind === 'car' || kind === 'truck'; 
+
 }
 
 /**
@@ -23,7 +24,7 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  return option1 > option2 ? `${option2} is clearly the better choice.` : `${option1} is clearly the better choice.`
 }
 
 /**
@@ -35,5 +36,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age < 3) {
+    return originalPrice * 80 / 100;
+  } else if (age >= 3 && age <= 10) {
+    return originalPrice * 70 / 100
+  } else {
+    return originalPrice / 2;
+  }
 }
