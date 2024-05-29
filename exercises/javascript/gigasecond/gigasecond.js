a75d1bd89253a35587abcde5bfc7bb1ac91b7d20
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-05-29 23:55:15 +0430

    complete gigasecond exercise

--- exercises/javascript/gigasecond/gigasecond.js
+++ exercises/javascript/gigasecond/gigasecond.js
@@ -3,6 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  const currentDate = new Date(date); 
+  const gigasecondTomilli = 1000000000000; 
+
+  const later = new Date(gigasecondTomilli + currentDate.getTime())
 };
