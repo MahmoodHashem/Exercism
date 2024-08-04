697025eba10a245a734ea35f3812143a6e11e684
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-08-04 13:57:56 +0430

    fix: solve the book-store exercise

--- exercises/javascript/book-store/book-store.js
+++ exercises/javascript/book-store/book-store.js
@@ -4,5 +4,25 @@
 //
 
 export const cost = (books) => {
-  throw new Error('Remove this statement and implement this function');
+  
+  let price = books.length * 800; 
+  let discount = {5:0.25, 4: 0.20, 3: 0.10, 2: 0.05}; 
+  let bookSet = []; 
+
+  while(books.length > 0){
+    bookSet.push(new Set(books).size); 
+    for (const book of new Set(books)) {
+      books.splice(books.indexOf(book), 1); 
+    }
+  }
+
+  while(bookSet.includes(3) && bookSet.includes(5)){
+    bookSet.splice(bookSet.indexOf(3), 1); 
+    bookSet.splice(bookSet.indexOf(5), 1); 
+    bookSet.push(4,4); 
+  }
+  
+  return price - bookSet.reduce((pre, curr) => pre + (discount[curr] || 0) * 800 * curr, 0)
+
+
 };
